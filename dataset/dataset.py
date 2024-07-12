075e9b9d8d26aec50012e859d2b2f35e7e857183
n0nSleeper <c0mputereng1neerstudent0@gmail.com>
2024-07-12 10:19:18 +0700

    First commit

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,7 +1,9 @@
 from PIL import Image
+import cv2 as cv
 from torch.utils.data import Dataset
 import torch
 from .constant import LABEL2ID
+from .transformation import inference_input
 
 class HENetdataset(Dataset):
     def __init__(self, img_paths, transform = None):
@@ -13,10 +15,11 @@ class HENetdataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
-        img = Image.open(img_path).convert('RGB')
-        #img_np = np.array(img, dtype=np.uint8)
+        img = cv.imread(img_path)
+        processed_img = inference_input(img)
+        input_img = Image.fromarray(cv.cvtColor(processed_img, cv.COLOR_BGR2RGB)) 
 
-        res = self.transform(img) if self.transform else img
+        res = self.transform(input_img) if self.transform else input_img
 
         label = LABEL2ID[img_path.split(",")[1][:-4]]
         return res, torch.as_tensor(label, dtype= torch.long)
